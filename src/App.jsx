89b59ce0306da47a6c1f3fcb3908cd1f7d4049d8
Bryan Fernando <154486475+Bryan-Fernando@users.noreply.github.com>
2024-10-23 01:21:17 -0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PaginaCurso1 from './pagina1/PaginaCurso1';
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/wordsworlds/",
-})
+  base: process.env.NODE_ENV === 'production' ? '/wordsworlds/' : '/', // Base diferente para produção e dev
+});
