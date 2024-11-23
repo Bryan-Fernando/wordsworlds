e53e49518b77748e27901992568b3acb887705ba
Bryan Fernando <154486475+Bryan-Fernando@users.noreply.github.com>
2024-11-23 08:49:42 -0300

    será que vai?

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wordsworlds/',  // isso garante que os caminhos estarão corretos para o GitHub Pages
-});
+})
